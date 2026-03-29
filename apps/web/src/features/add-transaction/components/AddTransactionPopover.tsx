1403d0c28e64eee29bfa7f0db0e82b95f049fb2e
wsameer <dev.wsameer@gmail.com>
2026-03-29 18:07:31 -0400

    [web] responsive dialog for transaction adding

--- apps/web/src/features/add-transaction/components/AddTransactionPopover.tsx
+++ apps/web/src/features/add-transaction/components/AddTransactionPopover.tsx
@@ -14,36 +14,73 @@ import {
   DrawerTitle,
 } from "@workspace/ui/components/drawer";
 import { TransactionsWrapper } from "./TransactionsWrapper";
+import { useIsMobile } from "@workspace/ui/hooks/use-mobile";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@workspace/ui/components/dialog";
 
 export function AddTransactionPopover() {
   const isOpen = useOpenCreateTransaction();
   const selectedTransactionType = useTransactionType();
+  const isMobile = useIsMobile();
 
   const { setOpenCreateTransaction } = useUiActions();
 
+  if (isMobile) {
+    return (
+      <Drawer open={isOpen} onOpenChange={setOpenCreateTransaction}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle className="text-left text-xl font-light">
+              Record
+              {selectedTransactionType === TransactionTypeSchema.enum.transfer
+                ? " a "
+                : " an "}
+              {selectedTransactionType}
+            </DrawerTitle>
+          </DrawerHeader>
+          <div className="mb-10 px-4">
+            <TransactionsWrapper />
+          </div>
+          <DrawerFooter>
+            <DrawerClose asChild>
+              <Button className="w-full" variant="secondary">
+                Cancel
+              </Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
   return (
-    <Drawer open={isOpen} onOpenChange={setOpenCreateTransaction}>
-      <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle className="text-left text-xl font-light">
+    <Dialog open={isOpen} onOpenChange={setOpenCreateTransaction}>
+      <DialogContent className="sm:max-w-[525px]">
+        <DialogHeader>
+          <DialogTitle className="text-left text-xl font-light">
             Record
             {selectedTransactionType === TransactionTypeSchema.enum.transfer
               ? " a "
               : " an "}
             {selectedTransactionType}
-          </DrawerTitle>
-        </DrawerHeader>
-        <div className="mb-10 px-4">
+          </DialogTitle>
+        </DialogHeader>
+        <div className="-mx-4 no-scrollbar max-h-[50vh] overflow-y-auto px-4">
           <TransactionsWrapper />
         </div>
-        <DrawerFooter>
-          <DrawerClose asChild>
-            <Button className="w-full" variant="secondary">
-              Cancel
-            </Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+        <DialogFooter>
+          <DialogClose
+            className="w-full"
+            render={<Button variant="secondary">Cancel</Button>}
+          />
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
